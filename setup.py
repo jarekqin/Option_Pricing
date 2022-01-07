7765d5c0d991146c6518d553f5267c4ac8194534
jarekqin <loveboa007@live.cn>
2022-01-07 23:54:29 +0800

    :hammer : update setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+import setuptools
+setuptools.setup(name='options_pricing_model_class',
+      version='0.0.1',
+      description='according to mastering python for finance 2nd edition',
+      url='None',
+      author='jarekqin',
+      packages=setuptools.find_packages(),
+      zip_safe=False)
